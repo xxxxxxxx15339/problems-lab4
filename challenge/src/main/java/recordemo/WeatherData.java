ce883b3635504c72e637eb9fbab1cb945e40f7f6
fahd-kalloubi <kalloubi.f@ucd.ac.ma>
2025-10-09 16:45:30 +0100

    done

--- challenge/src/main/java/recordemo/WeatherData.java
+++ challenge/src/main/java/recordemo/WeatherData.java
@@ -1,5 +1,23 @@
 package recordemo;
 
-public record WeatherData() {
-    
+public record WeatherData(double temperatureCelsius, String conditions) {
+
+    // Instance method to convert Celsius to Fahrenheit
+    public double temperatureFahrenheit() {
+        //
+    }
+
+    // Instance method to get a formatted summary string
+    public String getSummary() {
+        //
+    }
+
+    // Static factory method to create a WeatherData record from Fahrenheit
+    public static WeatherData fromFahrenheit(double tempFahrenheit, String conditions) {
+       //
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
